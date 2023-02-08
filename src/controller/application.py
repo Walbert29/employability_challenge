c6e8b156d4330971c88d637b8e187ff417be4434
WalbertN <walberto.rodrigez@hunty.com>
2023-02-08 10:35:32 -0500

    add controller create application

--- src/controller/application.py
+++ src/controller/application.py
@@ -1,8 +1,10 @@
 from fastapi import APIRouter, status
 
+from schemas.application import ApplicationSchema
 from src.services.application import (
     get_applications_by_user_id,
     get_applications_by_vacancy_id,
+    create_application
 )
 
 application_router = APIRouter(prefix="/application")
@@ -52,3 +54,25 @@ def get_application_by_vacancy_id(vacancy_id: str):
         List[ApplicationSchema]
     """
     return get_applications_by_vacancy_id(vacancy_id=vacancy_id)
+
+
+@application_router.post(
+    "/create",
+    tags=["Application"],
+    status_code=status.HTTP_200_OK,
+    summary="Create an application",
+)
+def create_match_application(application_data: ApplicationSchema):
+    """
+    This method is responsible for creating user applications
+
+    Method:
+        POST
+
+    Args:
+        application_data (ApplicationSchema): Application Data
+
+    Returns:
+        ApplicationSchema
+    """
+    return create_application(data_application_in=application_data)
